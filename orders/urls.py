717db8239fd7f780eec37c81d015dad4e2691423
BiTech5 <bikikc7@gmail.com>
2024-12-23 18:20:57 +0545

    template for order list and create order

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.list_orders, name='list_orders'),
+    path('create_order/',views.create_order,name="create_order")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
+from .models import Order
 # Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the orders index.")
+def list_orders(request: HttpRequest) -> HttpResponse:
+    data: dict[str, list[Order]] = {
+        'orders': Order.objects.all()
+    }
+    return render(request, 'orders/order_view.html', data)
 
+def create_order(request: HttpRequest) -> HttpResponse:
+    return render(request,'orders/create_order.html')
+def update_order(request: HttpRequest) -> HttpResponse:
+    pass
+def delete_order(request: HttpRequest) -> HttpResponse:
+    pass
